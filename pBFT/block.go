6ba8a51dd89ca38396604f9976ad9da0a230509e
Hoang Xuan Khoi <khoihoangxuan1508@gmail.com>
2024-12-06 15:30:50 +0700

    add func for blockchain

--- pBFT/block.go
+++ pBFT/block.go
@@ -1,9 +1,14 @@
+// package main
 package mainpBFT
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Block struct {
@@ -25,4 +30,90 @@ func NewBlock(previousBlockHash string, blockHeight int) Block {
 		BlockHeight:       blockHeight,
 		BlockHash:         blockHash,
 	}
+}
+
+type Blockchain struct {
+	Chain []Block
+	// chainFile string
+}
+
+func newBlockchain(chainFile string) error {
+	// Check coi tập tin tồn tại chưa
+	_, err := os.Stat(chainFile)
+	if err == nil {
+		// Nếu file đã tồn tại, không làm gì
+		return nil
+	}
+
+	// Nếu chưa có file thì tạo file
+	f, err := os.OpenFile(chainFile, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("could not create file: %w", err)
+	}
+	defer f.Close()
+
+	// Add block genesis
+	genesisBlock := NewBlock("", 0)
+	if err := appendBlockToBlockchain(chainFile, genesisBlock); err != nil {
+		return fmt.Errorf("could not write genesis block: %w", err)
+	}
+	return nil
+}
+
+func appendBlockToBlockchain(chainFile string, block Block) error {
+	f, err := os.OpenFile(chainFile, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(fmt.Sprintf("%s,%d,%s\n", block.PreviousBlockHash, block.BlockHeight, block.BlockHash))
+	return err
+}
+
+func loadBlockchainFromFile(chainFile string) ([]Block, error) {
+	// Mở file ở chế độ chỉ đọc
+	f, err := os.Open(chainFile)
+	if err != nil {
+		return nil, fmt.Errorf("could not open blockchain file: %w", err)
+	}
+	defer f.Close()
+
+	var blocks []Block
+
+	// Đọc từng dòng trong file và parse thành block
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		block, err := parseBlock(line)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse block: %w", err)
+		}
+		blocks = append(blocks, block)
+	}
+
+	// Kiểm tra lỗi quét file
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("could not read blockchain file: %w", err)
+	}
+
+	return blocks, nil
+}
+
+func parseBlock(line string) (Block, error) {
+	parts := strings.Split(line, ",")
+	if len(parts) != 3 {
+		return Block{}, fmt.Errorf("invalid block format")
+	}
+
+	blockHeight, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return Block{}, fmt.Errorf("invalid block height: %w", err)
+	}
+
+	return Block{
+		PreviousBlockHash: parts[0],
+		BlockHeight:       blockHeight,
+		BlockHash:         parts[2],
+	}, nil
 }
--- pBFT/pBFT_main.go
+++ pBFT/pBFT_main.go
@@ -2,11 +2,41 @@ package mainpBFT
 
 import "fmt"
 
-func main() {
-	genesisBlock := NewBlock("0", 0)
-	fmt.Printf("Genesis Block: %+v\n", genesisBlock)
+func MainpBFT() {
+	file := "blockchain.txt"
 
-	nextBlock := NewBlock(genesisBlock.BlockHash, 1)
-	fmt.Printf("Next Block: %+v\n", nextBlock)
+	// Khởi tạo blockchain, nếu file đã tồn tại thì không làm gì
+	err := newBlockchain(file)
+	if err != nil {
+		fmt.Println("Error initializing blockchain:", err)
+		return
+	}
 
+	// Đọc chuỗi block từ file
+	blocks, err := loadBlockchainFromFile(file)
+	if err != nil {
+		fmt.Println("Error loading blockchain:", err)
+		return
+	}
+
+	// Thêm block mới dô
+	newBlock := NewBlock(blocks[0].BlockHash, 1)
+	err = appendBlockToBlockchain(file, newBlock)
+	if err != nil {
+		fmt.Println("Error append block to file:", err)
+		return
+	}
+
+	// Đọc chuỗi block từ file
+	blocks, err = loadBlockchainFromFile(file)
+	if err != nil {
+		fmt.Println("Error loading blockchain:", err)
+		return
+	}
+
+	// In thông tin về các block
+	for _, block := range blocks {
+		fmt.Printf("BlockHeight: %d, Hash: %s, PrevHash: %s\n",
+			block.BlockHeight, block.BlockHash, block.PreviousBlockHash)
+	}
 }
